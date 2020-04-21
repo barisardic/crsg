8a2b3ff69bb43a718594f20d84702f78e8ae7440
Paledomain <mustafaugurkaan@gmail.com>
2020-04-21 20:16:11 +0300

    add cool cursor base

--- public/main.js
+++ public/main.js
@@ -66,3 +66,18 @@ function playPage(){
     // ...
   
 } */
+
+//mouse movements
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
