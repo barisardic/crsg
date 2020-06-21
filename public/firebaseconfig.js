9f2c506a49a817a1e2fdc65632962982959b1f99
barış ardıç <fbkazoid@gmail.com>
2020-06-21 06:15:58 +0300

    master config file

---
+++ public/firebaseconfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA0aonfduOwOfnPoCxZfuaDBcOufU2Zj_8",
+    authDomain: "crsg-protoype.firebaseapp.com",
+    databaseURL: "https://crsg-protoype.firebaseio.com",
+    projectId: "crsg-protoype",
+    storageBucket: "crsg-protoype.appspot.com",
+    messagingSenderId: "438209695879",
+    appId: "1:438209695879:web:41b459953d567130"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
